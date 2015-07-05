06c5c707dbc2d1c9edacaaa2f1b7d1ba998420ea
srkiley1 <srk2@att.net>
2015-07-05 11:53:31 -0700

    testing

--- js/app.js
+++ js/app.js
@@ -1,39 +1,63 @@
-
 $(document).ready(function() {
-  $('.ryu').mouseenter(function() {
-    $('.ryu-still').hide();
-    $('.ryu-ready').show();
-  })
-  .mouseleave(function() {
-$('.ryu-ready').hide();
-$('.ryu-still').show(); 
- })
+	$(".ryu").mouseenter(function() {
+		$(".ryu-still").hide();
+		$(".ryu-cool").hide();
+		$(".ryu-ready").show();
+	})
+	.mouseleave(function() {
+		$(".ryu-ready").hide();
+		$(".ryu-cool").hide();
+		$(".ryu-still").show();
+	})
+	.mousedown(function() {
+		playHadouken();
+		$(".ryu-ready").hide();
+		$(".ryu-cool").hide();
+		$(".ryu-still").hide();
+		$(".ryu-throwing").show();
+		$(".hadouken").finish().show()
+			.animate(
+				{"left": "300px"},
+				500,
+				function() {
+				$(this).hide();
+				$(this).css("left", "-212px");
+			}
+		);
 
-  
-  .mousedown(function() {
-  playHadouken();
-  $('.ryu-ready').hide();
-  $('.ryu-throwing').show();
-  $('.hadouken').finish().show()
-  .animate(
-    {'left': '1020px'},
-    500,
-    function() {
-      $(this).hide();
-      $(this).css('left', '520px');
-    });
-  })
+	})
+	.mouseup(function() {
+		$(".ryu-throwing").hide();
+		$(".ryu-cool").hide();
+		$(".ryu-still").hide();
+		$(".ryu-ready").show();
+	})
 
-    // animate hadouken to the right of the screen
-  
-  .mouseup(function() {
-  $('.ryu-throwing').hide();
-  $('.ryu-ready').show();
+	$("body").keydown(function(e) {
+		if (e.which == 88) {
+			$(".ryu-still").hide();
+			$(".ryu-ready").hide();
+			$(".ryu-cool").show();
+			$(".hadouken").hide();
+		}
+	})
+
+	$("body").keyup(function(e) {
+		if (e.which == 88) {
+			$(".ryu-still").show();
+			$(".ryu-ready").hide();
+			$(".ryu-cool").hide();
+			$(".hadouken").hide();
+
+		}
+	})
+
+
 });
-});  
-function playHadouken () {
-  $('#hadouken-sound')[0].volume = 0.5;
-  $('#hadouken-sound')[0].load();
-  $('#hadouken-sound')[0].play();
+
+function playHadouken() {
+	$("#hadouken-sound")[0].volume = 0.5;
+	$("#hadouken-sound")[0].load();
+	$("#hadouken-sound")[0].play();
 }
-     
+
